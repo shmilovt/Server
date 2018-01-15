143e4761e4ca0980efc4a4bc5e976aa62925bf15
nofarna <nofar.naftchi@gmail.com>
2018-01-15 21:23:15 +0200

    AnalyzedDSTest

--- src/test/java/il/ac/bgu/finalproject/server/Domain/NLPHandlers/AnalyzedDSTest.java
+++ src/test/java/il/ac/bgu/finalproject/server/Domain/NLPHandlers/AnalyzedDSTest.java
@@ -13,10 +13,11 @@ public class AnalyzedDSTest {
     public void insert() {
         EnvList env = new EnvList("להשכרה דירה מהממת ברחוב ברנפלד 13, בעל דירה מקסים! הדירה מרוהטת קומפלט, 4 חדרים, כולל מטבח מאובזר, רק 900 שח בחודש! כדאי מאוד! לפרטים, נופר- 053-3391800");
         AnalyzedDS ds= new AnalyzedDS(env);
-        ds.Insert(Classify.PHONE,1,"0533391800");
+        ds.Insert(Classify.PHONE,1,"0533391822");
         List<String> ex= new ArrayList<String>();
+        ex.add("0533391822");
         ex.add("0533391800");
-        assertEquals(ds.GetResultsByClassify(Classify.PHONE),ex);
+        assertEquals(ex,ds.GetResultsByClassify(Classify.PHONE));
     }
 
 
@@ -24,10 +25,9 @@ public class AnalyzedDSTest {
     public void remove() {
         EnvList env = new EnvList("להשכרה דירה מהממת ברחוב ברנפלד 13, בעל דירה מקסים! הדירה מרוהטת קומפלט, 4 חדרים, כולל מטבח מאובזר, רק 900 שח בחודש! כדאי מאוד! לפרטים, נופר- 053-3391800");
         AnalyzedDS ds= new AnalyzedDS(env);
-        ds.Insert(Classify.PHONE,1,"0533391800");
-        ds.Remove(Classify.PHONE,1,"0533391800");
+        ds.Remove(Classify.PHONE,5,"0533391800");
         List<String> ex= new ArrayList<String>();
-        assertEquals(ds.GetResultsByClassify(Classify.PHONE),ex);
+        assertEquals(ex,ds.GetResultsByClassify(Classify.PHONE));
     }
 
     @Test
@@ -36,9 +36,7 @@ public class AnalyzedDSTest {
         AnalyzedDS ds= new AnalyzedDS(env);
         //System.out.println(ds.GetEnvsIndex(Classify.PHONE));
         List<Integer> ex= new ArrayList<Integer>();
-        assertEquals(ex,ds.GetEnvsIndex(Classify.PHONE));
-        ds.Insert(Classify.PHONE,1,"0533391800");
-        ex.add(1);
+        ex.add(5);
         assertEquals(ex,ds.GetEnvsIndex(Classify.PHONE));
 
     }
@@ -48,11 +46,7 @@ public class AnalyzedDSTest {
         EnvList env = new EnvList("להשכרה דירה מהממת ברחוב ברנפלד 13, בעל דירה מקסים! הדירה מרוהטת קומפלט, 4 חדרים, כולל מטבח מאובזר, רק 900 שח בחודש! כדאי מאוד! לפרטים, נופר- 053-3391800, יכולים גם לתאם לראות את הדירה עם נועה- 053-3311010");
         AnalyzedDS ds= new AnalyzedDS(env);
         List<String> ex= new ArrayList<String>();
-        assertEquals(ex,ds.GetResultsByClassify(Classify.PHONE));
-        ds.Insert(Classify.PHONE,6,"0533391800");
         ex.add("0533391800");
-        assertEquals(ex,ds.GetResultsByClassify(Classify.PHONE));
-        ds.Insert(Classify.PHONE,7,"0533311010");
         ex.add("0533311010");
         assertEquals(ex,ds.GetResultsByClassify(Classify.PHONE));
     }
@@ -62,14 +56,11 @@ public class AnalyzedDSTest {
         EnvList env = new EnvList("להשכרה דירה מהממת ברחוב ברנפלד 13, בעל דירה מקסים! הדירה מרוהטת קומפלט, 4 חדרים, כולל מטבח מאובזר, רק 900 שח בחודש! כדאי מאוד! לפרטים, נופר- 053-3391800, יכולים גם לתאם לראות את הדירה עם נועה- 053-3311010");
         AnalyzedDS ds= new AnalyzedDS(env);
         List<String> ex= new ArrayList<String>();
-        assertEquals(ex,ds.GetResultsByClassifyAndIndex(Classify.PHONE,6));
-        ds.Insert(Classify.PHONE,6,"0533391800");
-        ds.Insert(Classify.PHONE,7,"0533311010");
         ex.add("0533391800");
-        assertEquals(ex,ds.GetResultsByClassifyAndIndex(Classify.PHONE,6));
+        assertEquals(ex,ds.GetResultsByClassifyAndIndex(Classify.PHONE,5));
         ex.remove("0533391800");
         ex.add("0533311010");
-        assertEquals(ex,ds.GetResultsByClassifyAndIndex(Classify.PHONE,7));
+        assertEquals(ex,ds.GetResultsByClassifyAndIndex(Classify.PHONE,6));
     }
 
     /*
