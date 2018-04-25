e866c5724e51064a35b3d69cae9661e65e79e481
nofarna <nofar.naftchi@gmail.com>
2018-04-25 14:40:03 +0300

    get from DB functions are good

---
+++ src/test/java/il/ac/bgu/finalproject/server/Domain/DomainObjects/UserSearchingUtils/SearchAlgorithmTest.java
@@ -0,0 +1,14 @@
+package il.ac.bgu.finalproject.server.Domain.DomainObjects.UserSearchingUtils;
+
+import org.junit.Test;
+
+public class SearchAlgorithmTest {
+
+    @Test
+    public void filterIntersection() {
+    }
+
+    @Test
+    public void filterMoreResults() {
+    }
+}
